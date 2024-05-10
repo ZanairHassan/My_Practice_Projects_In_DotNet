6f911fc5e115f7b6acd53845e38643dce5506d4d
ZanairHuboWeb <zanair.hassan@huboweb.com>
2024-05-10 15:23:00 +0500

    I have successfully debug and test the Patient Utility
    As the application Successfully check the Zip Folder  search the required File with in the Zip folder then read the data in the searched file and Write it on the Sql Table Named MON.Patient with the Help of Store Procedure Named InsertPatient.

--- PatientUtility/PatientUtility/obj/Debug/net8.0/PatientUtility.AssemblyInfo.cs
+++ PatientUtility/PatientUtility/obj/Debug/net8.0/PatientUtility.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientUtility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e493477c5796d578e62ba81afc647579b354025c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientUtility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientUtility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
