4373ee71ab0705a1dacc1441fd216d8c8ec729de
ZanairHuboWeb <zanair.hassan@huboweb.com>
2024-05-27 13:56:30 +0500

    Adding the connection strings and generating the SuperHero Model

---
+++ SuperHeroApi-DotNet8/SuperHeroApi-DotNet8/Controllers/SuperHeroController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroApi_DotNet8.Entities;
+using System.Security.Cryptography;
+
+namespace SuperHeroApi_DotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeros()
+        {
+            var heros = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id= 1,
+                    Name="IronMan",
+                    FirstName="Jonny",
+                    LastName="Sin",
+                    Place="California"
+                }
+            };
+            return Ok(heros);
+        }
+    }
+}
